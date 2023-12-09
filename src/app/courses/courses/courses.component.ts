2a1b28c4f1a67d7c89d13ec4be686c57aa68af01
Sérgio Lemos <contato@sergiolemos.com>
2023-12-09 10:15:40 -0300

    finalizacao da listagem

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog'
 
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
 
 @Component({
   selector: 'app-courses',
@@ -9,14 +15,30 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    {_id: '1', name: 'Angular', category: 'Frontend'}
-  ];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() { }
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('erro ao carregar cursos.')
+          return of([]);
+        })
+      );
+  }
 
   ngOnInit(): void {
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    })
   }
 
 }
---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap,first, delay } from 'rxjs/operators';
+
+import { Course } from '../model/course';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  list(){
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      delay(1000),
+      tap(courses => console.log(courses))
+    );
+  }
+}
